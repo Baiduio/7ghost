5271e5b7afd20964d4a22e0a391cbfd5281a6940
Baiduio <31921872+Baiduio@users.noreply.github.com>
2018-12-16 16:18:48 +0800

    添加修改后的7ghost

---
+++ _admin/data/config.php
@@ -0,0 +1,20 @@
+<?php return array (
+  'password' => '123456',
+  'host' => 'http://www.google.com/',
+  'replaceDomain' => '0',
+  'relativeHTML' => '0',
+  'relativeCSS' => '0',
+  'static' => '0',
+  'diyStatic' => 'css|js|html|png|jpg',
+  'cookies' => '0',
+  'agent' => '0',
+  'referer' => '0',
+  'ip' => '0',
+  'diyAgent' => '',
+  'replaces' => 
+  array (
+  ),
+  'pages' => 
+  array (
+  ),
+);
