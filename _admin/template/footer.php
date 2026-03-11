5ffb24488b457f2c8b209ef438b4478911740c97
Baiduio <31921872+Baiduio@users.noreply.github.com>
2026-03-11 20:49:47 +0800

    修复2.0.0.2的bug
    更新联系方式以及项目地址

--- _admin/template/footer.php
+++ _admin/template/footer.php
@@ -1,6 +1,6 @@
 	<div class="bottom">
 		<div id="copyright">	
-		&copy;2011 7eam - <a href='http://code.google.com/p/7ghost/'>官方网站</a> - <a href='http://www.7eam.com'>官方论坛</a> 
+		&copy;2026 Baiduio - <a href='https://github.com/baiduio/7ghost'>项目地址</a> - <a href='mailto:baiduio@qq.com'>联系作者</a> 
 		</div>
 	</div>
 </div><!-- end .doc -->
